0ea7d0e66506404a9b0ed887ab0fe2b396790ef1
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-04-17 16:23:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -45,5 +45,22 @@ $(document).ready(function () {
         $('body').toggleClass('hidden');
         $('.header__nav').toggleClass('show');
     })
+    
+
+    // Impact page  - anchors
+    document.querySelectorAll('.impactgr-item[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+      
+          const targetId = this.getAttribute('href');
+          const target = document.querySelector(targetId);
+      
+          if (target) {
+            target.scrollIntoView({
+              behavior: 'smooth'
+            });
+          }
+        });
+      });
 
 })
